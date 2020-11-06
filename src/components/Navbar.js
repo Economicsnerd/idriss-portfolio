957021fc5f017fdb528a28d45647d6c28b14ab74
Idriss Amraoui <idrissamraoui@protonmail.com>
2020-11-06 09:05:34 -0500

    Fixed Navigation issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import NavItem from './NavItem';
 
-export class Navbar extends Component {
-    constructor(props){
+class Navbar extends Component {
+    constructor(props)
+    {
         super(props);
-        this.state = {'NavItemActive': ''}
+        this.state={
+            'NavItemActive':''
+        }
     }
-
-    // Add CSS class .active to NavItem
-    activeitem = (element) => {
-        if(this.state.NavItemId.length > 0){
+    activeitem=(x)=>
+    {
+        if(this.state.NavItemActive.length>0){
             document.getElementById(this.state.NavItemActive).classList.remove('active');
         }
-        this.setState({'NavItemActive': element}, () => {
+        this.setState({'NavItemActive':x},()=>{
             document.getElementById(this.state.NavItemActive).classList.add('active');
-        })
-    }
+        });
+    };
     render() {
         return (
             <nav>
-                <ul>
-                    <NavItem item="Home" tolink="/" activec={this.activeitem}></NavItem>
-                    <NavItem item="About" tolink="/about" activec={this.activeitem}></NavItem>
-                    <NavItem item="Education" tolink="/education" activec={this.activeitem}></NavItem>
-                    <NavItem item="Skills" tolink="/skills" activec={this.activeitem}></NavItem>
-                    <NavItem item="Contact" tolink="/contact" activec={this.activeitem}></NavItem>                    
-                </ul>
+            <ul>
+            <NavItem item="Home" tolink="/"  activec={this.activeitem}></NavItem>
+            <NavItem item="About" tolink="/about"  activec={this.activeitem}></NavItem>
+            <NavItem item="Education" tolink="/education"  activec={this.activeitem}></NavItem>
+            <NavItem item="Skills" tolink="/skills"  activec={this.activeitem}></NavItem>
+            <NavItem item="Contact" tolink="/contact"  activec={this.activeitem}></NavItem>
+            </ul>
             </nav>
-        )
+            )
+        }
     }
-}
-
-export default Navbar
+    
+    export default Navbar
