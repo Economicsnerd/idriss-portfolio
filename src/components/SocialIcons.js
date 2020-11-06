1dcb15b43f67084a71afbd636b6c06725513350f
Idriss Amraoui <idrissamraoui@protonmail.com>
2020-11-06 08:58:12 -0500

    Fixed Fontawesome

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -5,10 +5,7 @@ export class SocialIcons extends Component {
     render() {
         return (
             <div className="social">
-                <a href="https://github.com/Economicsnerd" target="_blank">
-                    
-                    </a>
-                    <FontAwesomeIcon icon={['fab', 'github']}/>
+                <a href="https://github.com/Economicsnerd" target="_blank"><i class="fab fa-github"></i></a>
                 <a href="https://www.instagram.com/idrissdebeard/Li" target="_blank"><i class="fab fa-instagram"></i></a>
                 <a href="https://www.linkedin.com/in/idriss-amraoui-036176123/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
             </div>
