43f72c74c30f424a1c1d47d2f00e11a158fc8dc3
Idriss Amraoui <idrissamraoui@protonmail.com>
2020-11-13 16:49:35 -0500

    Fixed profile picture issue

--- src/content/Contact.js
+++ src/content/Contact.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
-import SocialIcons from '../components/SocialIcons'
-import Gmail from '../img/gmail.webp'
+import React, { Component } from 'react';
+import SocialIcons from '../components/SocialIcons';
+import Gmail from '../img/gmail.webp';
 
 export class Contact extends Component {
-    render() {
-        return (
-                <div className="condiv">
-                    <h1 className="subtopic">Contact Me</h1>
-                    <div className="widecard">
-                        <div className="education-text">
-                            <h3>Email   :   idrisscodes@gmail.com</h3>
-                            <h3>Instagram   :   @economicsnerd</h3>
-                        </div>
-                        <img src={Gmail} alt="Gmail icon" className="school-logo"/>                        
-                    </div>
-                    <SocialIcons />
-                </div>   
-        )
-    }
+  render() {
+    return (
+      <div className="condiv">
+        <h1 className="subtopic">Contact Me</h1>
+        <div className="widecard">
+          <div className="education-text">
+            <h3>Email : idrisscodes@gmail.com</h3>
+            <h3>Instagram : @economicsnerd</h3>
+          </div>
+          <img src={Gmail} alt="Gmail icon" className="school-logo" />
+        </div>
+        <SocialIcons />
+      </div>
+    );
+  }
 }
 
-export default Contact
+export default Contact;
--- src/content/Home.js
+++ src/content/Home.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react'
-import ReactTypingEffect from 'react-typing-effect'
-import profilepic from '../img/Idriss_Photo.jpg'
-import SocialIcons from '../components/SocialIcons'
+import React, { Component } from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import profilepic from '../img/Idriss_Photo.jpg';
+import SocialIcons from '../components/SocialIcons';
 
 export class Home extends Component {
-    render() {
-        return (
-            <div className="condiv home">
-            <img src={profilepic} alt="Idriss' profile picture" className="profilepic"></img>
-            <ReactTypingEffect className="typingeffect" text={['Thank you for stopping by!', 'My name is Idriss', 'And I am a Web Developper', 'Check out my profile']} speed={200} eraseDelay={300}/>
-            <SocialIcons />
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div className="condiv home">
+        <img
+          src={profilepic}
+          alt="Idriss' profile picture"
+          className="profilepic"
+        ></img>
+        <ReactTypingEffect
+          className="typingeffect"
+          text={[
+            'Thank you for stopping by!',
+            'My name is Idriss',
+            'And I am a Web Developper',
+            'Check out my profile',
+          ]}
+          speed={200}
+          eraseDelay={300}
+        />
+        <SocialIcons />
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
